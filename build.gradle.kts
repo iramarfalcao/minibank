6f755c1a8f81b7238a30d7dca677e725738ca07c
iramafalcao <iramar.j@hotmail.com>
2022-01-16 18:57:42 -0300

    refactor and tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security:2.6.2")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation ("org.springdoc:springdoc-openapi-ui:1.6.4")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
