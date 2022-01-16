3f4e54357a10cd465c7456b37f53c1e39a9a5748
iramafalcao <iramar.j@hotmail.com>
2022-01-15 23:14:40 -0300

    first commit

---
+++ src/test/kotlin/com/iramarfalcao/minibank/MinibankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iramarfalcao.minibank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinibankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
