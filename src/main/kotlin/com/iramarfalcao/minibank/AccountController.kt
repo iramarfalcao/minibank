8182dd2c37596bcc93f2d504b4ecec56f8b8bcf2
iramafalcao <iramar.j@hotmail.com>
2022-01-16 15:26:34 -0300

    first commit

--- src/main/kotlin/com/iramarfalcao/minibank/AccountController.kt
+++ src/main/kotlin/com/iramarfalcao/minibank/AccountController.kt
@@ -1,5 +1,6 @@
 package com.iramarfalcao.minibank
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -18,5 +19,25 @@ class AccountController(
     fun getById(@PathVariable id: Long): ResponseEntity<Account> =
         accountRepository.findById(id).map {
             ResponseEntity.ok(it)
+        }.orElse(ResponseEntity.notFound().build())
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody account: Account
+    ): ResponseEntity<Account> = accountRepository.findById(id).map {
+        val accountToUpdate = it.copy(
+            name = account.name,
+            document = account.document,
+            phone = account.phone
+        )
+        ResponseEntity.ok(accountRepository.save(accountToUpdate))
+    }.orElse(ResponseEntity.notFound().build())
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
+        accountRepository.findById(id).map {
+            accountRepository.delete(it)
+            ResponseEntity<Void>(HttpStatus.OK)
         }.orElse(ResponseEntity.notFound().build())
 }
